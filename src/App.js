4aa431225c5dbd1f6352578d1e4c95629d4662b1
Ricky Marasigan <rickysoliman@gmail.com>
2022-11-28 11:28:36 -0800

    api functioning in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import { useState } from 'react';
+// import { openai } from 'openai';
+import { Configuration, OpenAIApi } from 'openai';
 import './App.css';
+
+// const { Configuration, OpenAIApi } = require('openai');
+const configuration = new Configuration({
+  apiKey: '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', // need to hide this in a variable later
+});
+const openai = new OpenAIApi(configuration);
 
 const App = () => {
   const [imgSrc, setImgSrc] = useState('');
 
-  const handleClick = () => {
-    setImgSrc('https://lh3.googleusercontent.com/ogw/AOh-ky37qxWvGGL7ZZs09PtotH_jzuyCkajWrEwactyWYQ=s64-c-mo');
+  const generateImage = async () => {
+    const response = await openai.createImage({
+      prompt: 'a white siamese cat',
+      n: 1,
+      size: '1024x1024',
+    });
+    console.log({ response });
+    setImgSrc(response.data.data[0].url);
   }
 
   return (
@@ -22,7 +36,7 @@ const App = () => {
         ></input>
         <button
           className="App-button"
-          onClick={handleClick}
+          onClick={generateImage}
         >
           Submit
         </button>
